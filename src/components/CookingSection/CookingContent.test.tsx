ab362982d67912a036df16cc81b4406679633cd2
Sebastian Hernandez Caro <sebashdm@gmail.com>
2025-06-27 18:38:55 -0500

    test: added unit test

--- src/components/CookingSection/CookingContent.test.tsx
+++ src/components/CookingSection/CookingContent.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import CookingContent from './CookingContent'
 
 describe('CookingContent', () => {
